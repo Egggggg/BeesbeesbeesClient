cb6f591bfdc7d01d5b4fddb4fa42b64ea8f21d3e
Bee <mrhumanman2@gmail.com>
2021-08-19 16:22:55 -0400

    Add /dev_build to build files for dev mode

---
+++ detrista/dev_build/build.js
@@ -0,0 +1,14 @@
+const replace = require("replace-in-file");
+
+const options = {
+	files: "../*.html",
+	from: /https:\/\/socket.beesbeesbees.me/g,
+	to: "https://socket.beesbeesbees.me:3000"
+}
+
+try {
+	let changedFiles = replace.sync(options);
+	console.log(`Modified ${changedFiled.length} files`);
+} catch (error) {
+	console.error("An error occurred: ", error);
+}
