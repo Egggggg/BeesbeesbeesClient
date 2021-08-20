3f81552b4801b4bc6a09c1750989bb278309725d
Bee <35084270+Egggggg@users.noreply.github.com>
2021-08-20 16:08:04 -0400

    Add docstrings to client.js

--- detrista/scripts/client.js
+++ detrista/scripts/client.js
@@ -19,37 +19,72 @@ class Client {
         this.engineP1 = new Engine(p1Board, 1);
         this.engineP2 = new Engine(p2Board, 2);
 
-		socket.on("room joined", (p1Board, p2Board, p1Detritan, p2Detritan) => {
-			this.engineP1.sync(p1Board, p1Detritan);
-			this.engineP2.sync(p2Board, p2Detritan);
-		});
+		socket.on("room joined", this.onJoined);
 
-		socket.on("move valid", (sequence, detritan, next, playerNum, clearedLines, points) => {
-			const engine = this[`engineP${playerNum}`];
+		socket.on("move valid", this.onValid);
 
-			engine.sequenceInput.value = "";
-
-			engine.active.erase();
-			engine.spawn(detritan);
-			
-			engine.doSequence(sequence, 50).then(() => {
-				engine.spawn(next);
-
-				console.log(clearedLines);
-
-				if (clearedLines.length > 0) {
-					engine.clear(clearedLines);
-					engine.setPoints(engine.points + points);
-				}
-			});
-		});
-
-		socket.on("move invalid", (message, detritan, board, playerNum) => {
-			showWarning(message);
-
-			const engine = this[`engineP${playerNum}`];
-
-			engine.sync(board, detritan);
-		});
+		socket.on("move invalid", this.onInvalid);
     }
+
+	/**
+	 * Fires on "room joined" event
+	 * Syncs the boards and Detritans with the server response
+	 * @function onJoined
+	 * @param {number[][]} p1Board 
+	 * @param {number[][]} p2Board 
+	 * @param {Object} p1Detritan 
+	 * @param {Object} p2Detritan 
+	 */
+	onJoined(p1Board, p2Board, p1Detritan, p2Detritan) {
+		this.engineP1.sync(p1Board, p1Detritan);
+		this.engineP2.sync(p2Board, p2Detritan);
+	}
+
+	/**
+	 * Fires on "move valid" event
+	 * Performs the move and sets the new Detritan
+	 * @function onValid
+	 * @param {string} sequence 
+	 * @param {Object} detritan 
+	 * @param {Object} next 
+	 * @param {number} playerNum 
+	 * @param {number} clearedLines 
+	 * @param {number} points 
+	 */
+	onValid(sequence, detritan, next, playerNum, clearedLines, points) {
+		const engine = this[`engineP${playerNum}`];
+
+		engine.sequenceInput.value = "";
+
+		engine.active.erase();
+		engine.spawn(detritan);
+		
+		engine.doSequence(sequence, 50).then(() => {
+			engine.spawn(next);
+
+			console.log(clearedLines);
+
+			if (clearedLines.length > 0) {
+				engine.clear(clearedLines);
+				engine.setPoints(engine.points + points);
+			}
+		});
+	}
+
+	/**
+	 * Fires on "move invalid" event
+	 * Displays the error message and resets the Detritan and board
+	 * @function onInvalid
+	 * @param {string} message 
+	 * @param {Object} detritan 
+	 * @param {number[][]} board 
+	 * @param {number} playerNum 
+	 */
+	onInvalid(message, detritan, board, playerNum) {
+		showWarning(message);
+
+		const engine = this[`engineP${playerNum}`];
+
+		engine.sync(board, detritan);
+	}
 }
