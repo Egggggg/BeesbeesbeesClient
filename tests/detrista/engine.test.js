3db5a4c3e8163870cd15f9b773333df7952e0f22
Egggggg <mrhumanman2@gmail.com>
2021-08-28 23:54:23 -0400

    feat(Test): Add first few tests

--- tests/detrista/engine.test.js
+++ tests/detrista/engine.test.js
@@ -1,10 +1,11 @@
-const { Engine } = require("../../detrista/scripts/engine")
-const shapes = require("shapes.json");
+const { Engine } = require("../../detrista/scripts/engine");
+const { Square } = require("../../detrista/scripts/square");
+const shapes = require("./shapes.json");
 
 let engine;
 
 beforeEach(() => {
-	let board;
+	let board = [];
 	let columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 	for (let y = 0; y < 20; y++) {
@@ -14,8 +15,43 @@ beforeEach(() => {
 	engine = new Engine(board);
 });
 
-describe("test I detritan", () => {
-	test("check move left", () => {
+describe("move I left", () => {
+	test("check move I left", () => {
+		engine.spawn(shapes.i);
+		engine.moveLeftRight(engine, -1);
 
+		for (let i = 2; i < 6; i++) {
+			expect(engine.board[1][i].getColorIndex()).toBe(shapes.i.color);
+		}
+	});
+
+	test("check test move I left", () => {
+		engine.spawn(shapes.i);
+		engine.testMoveLeftRight(engine, -1);
+
+		expect(engine.active.shape.test[1]).toBe(2);
+	});
+
+	test("check move I left out of bounds", () => {
+		engine.spawn(shapes.i);
+		
+		engine.doSequence("aaaaa".split(""));
+
+		for (let i = 0; i < 4; i++) {
+			expect(engine.board[1][i].getColorIndex()).toBe(shapes.i.color);
+		}
+	});
+});
+
+describe("move J right", () => {
+	test("check move J right", () => {
+		engine.spawn(shapes.j);
+		engine.moveLeftRight(engine, 1);
+
+		expect(engine.board[0][4].getColorIndex()).toBe(shapes.j.color);
+
+		for (let i = 4; i < 7; i++) {
+			expect(engine.board[1][i].getColorIndex()).toBe(shapes.j.color);
+		}
 	});
 });
